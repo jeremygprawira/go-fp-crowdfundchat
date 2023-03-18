0ac7c6bff65adfca84ac50aa55f9cffad3fcd944
Jeremy <jeremygprawira@gmail.com>
2023-03-18 22:25:01 +0700

    create project usecase for project list

--- usecase/usecase_project.go
+++ usecase/usecase_project.go
@@ -1,1 +1,36 @@
 package usecase
+
+import (
+	"go-fp-crowdfundchat/model"
+	"go-fp-crowdfundchat/repository"
+)
+
+type ProjectService interface{
+	PostProjectList(userID int) ([]*model.Project, error)
+}
+
+type projectUsecase struct {
+	repo repository.UserRepository
+}
+
+func NewProjectUsecase(repo repository.UserRepository) *projectUsecase {
+	return &projectUsecase{repo}
+}
+
+func (u *projectUsecase) PostProjectList(userID int) ([]*model.Project, error) {
+	if userID != 0 {
+		projects, err := u.repo.FindProjectByUserID(userID)
+		if err != nil {
+			return projects, err
+		}
+
+		return projects, nil
+	}
+
+	projects, err := u.repo.FindAllProject()
+	if err != nil {
+		return projects, err
+	}
+
+	return projects, nil
+}
