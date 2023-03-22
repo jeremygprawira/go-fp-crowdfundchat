cbbd622d50fdb85bf46a6a1b57c8f57f6ccdd006
Jeremy <jeremygprawira@gmail.com>
2023-03-22 23:48:59 +0700

    add model transaction

--- model/transaction.go
+++ model/transaction.go
@@ -1,1 +1,14 @@
 package model
+
+import "time"
+
+type Transaction struct {
+	ID						int
+	ProjectID				int
+	UserID					int
+	Amount					int
+	Status					string
+	Code					string
+	CreatedAt 				time.Time		`json:"created_at"`
+	UpdatedAt 				time.Time		`json:"updated_at"`
+}
