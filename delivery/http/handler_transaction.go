19e4e387c79cd4c8607dd7af467924568fcb40af
Jeremy <jeremygprawira@gmail.com>
2023-03-28 00:16:27 +0700

    get transaction by user id

--- delivery/http/handler_transaction.go
+++ delivery/http/handler_transaction.go
@@ -53,3 +53,48 @@ func (h *TransactionHandler) ProjectTransactionList(c *gin.Context) {
 		"transactions": transactionList,
 	})
 }
+
+func (h *TransactionHandler) UserTransactionList(c *gin.Context) {
+	userID := c.GetInt("currentUser")
+	
+	transaction, err := h.transactionUsecase.GetTransactionByUserID(userID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"responseCode": "40021",
+			"responseMessage": "Failed to retrieve the user's transaction",
+		})
+		return
+	}
+
+	var projectImagesList *model.ProjectImagesTransactionListResponse
+	for _, p := range transaction {
+		if len(p.Project.ProjectImages) > 0 {
+			projectImagesList = &model.ProjectImagesTransactionListResponse{
+				Name: p.Project.ProjectTitle,
+				ImageURL: p.Project.ProjectImages[0].FileName,
+			}
+		} else if len(p.Project.ProjectImages) == 0 {
+			projectImagesList = &model.ProjectImagesTransactionListResponse{
+				Name: p.Project.ProjectTitle,
+				ImageURL: "",
+			}
+		}
+	}
+
+	userTransactionList := make([]*model.UserTransactionListResponse, len(transaction))
+	for i, p := range transaction {
+		userTransactionList[i] = &model.UserTransactionListResponse{
+			ID: p.ID,
+			Amount: p.Amount,
+			Status: p.Status,
+			CreatedAt: p.CreatedAt,
+			Project: *projectImagesList,
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"responseCode": "20000",
+		"responseMessage": "Request has been successfully sent.",
+		"transactions": userTransactionList,
+	})
+}
